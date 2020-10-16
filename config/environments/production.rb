e5a6ea3b0610708024eaf4cd7de4341118c7a69c
vasanth-krrish <vasanthkrrish96@gmail.com>
2020-10-16 09:43:43 +0530

    a

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,20 +110,6 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: "vasanthkrrsh96@gmail.com",
-    password: "AmmaSuba@72",
-    domain: "smtp.gmail.com",
-    openssl_verify_mode: "none",
-  }
+  
 
 end
